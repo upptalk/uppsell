98d99f008fbaa0875b7092e7d7e5df9c5215cc40
Adam Hayward <adam@happy.cat>
2014-04-04 15:49:52 +0200

    Adding setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "uppsell",
+    version = "0.1",
+    author = "Adam Hayward",
+    author_email = "adam@happy.cat",
+    description = ("A Flask-based e-commerce API "\
+            "and a django-backed admin for managing them."),
+    license = "MIT",
+    keywords = "example documentation tutorial",
+    url = "https://github.com/upptalk/uppsell",
+    packages=['uppsell'],
+    long_description="See https://github.com/upptalk/uppsell",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Flask",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
