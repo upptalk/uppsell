d450d987d8479eb676ef3fa3be549e8bde275ad4
Adam Hayward <adam@happy.cat>
2014-07-21 10:37:13 +0200

    Bug: empty property in format_order()

--- uppsell/uppsell_api/resources.py
+++ uppsell/uppsell_api/resources.py
@@ -31,7 +31,7 @@ def format_listing(listing, quantity = None):
     prod_dict['shipping'] = listing_dict['shipping']
     prod_dict['tax_rate'] = listing.tax_rate.rate
     for k in ('name', 'title', 'subtitle', 'description', 'features'):
-        if listing_dict[k].strip(): prod_dict[k] = listing_dict[k]
+        if listing_dict[k]: prod_dict[k] = listing_dict[k]
     if prod_dict['features']:
         prod_dict['features'] = [f for f in \
                 [f.strip() for f in prod_dict['features'].split("\n")] if f]
